4e02bdffd5b1520b8005e7a56f655172f43060a4
Alexandre <alexandre.s.o1994@hotmail.com>
2024-12-02 15:08:18 -0300

    adicionado a funcao de mostrar favoritos

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -4,8 +4,25 @@ import 'package:loja_flutter_curso/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
+  bool _showFavoriteOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    } else {
+      return [..._items];
+    }
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/pages/product/products_overview_pages.dart
+++ lib/pages/product/products_overview_pages.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:loja_flutter_curso/components/product_grid.dart';
+import 'package:loja_flutter_curso/models/product_list.dart';
+import 'package:provider/provider.dart';
+
+enum FilterOption {
+  favorite,
+  all,
+}
 
 class ProductsOverviewPages extends StatelessWidget {
   const ProductsOverviewPages({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         actions: [
           PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
             itemBuilder: (context) => [
               const PopupMenuItem(
-                value: 0,
+                value: FilterOption.favorite,
                 child: Text('Somente Favoritos'),
               ),
               const PopupMenuItem(
-                value: 1,
+                value: FilterOption.all,
                 child: Text('Todos'),
               ),
             ],
+            onSelected: (FilterOption selectedValue) {
+              if (selectedValue == FilterOption.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
           )
         ],
         backgroundColor: Theme.of(context).primaryColor,
